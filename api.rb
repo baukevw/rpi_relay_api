c4e6a2c5335f5ce654f577c9daeb4184b208ce26
baukevw <bauke@baukevanwieren.nl>
2017-05-29 11:15:31 +0200

    pin number

--- api.rb
+++ api.rb
@@ -11,7 +11,7 @@ end
 class RelayAPI < Sinatra::Base
   include PiPiper
 
-  PINS = [17,18,27,22,23,24,25,04]
+  PINS = [17,18,27,22,23,24,25,4]
   PINS.each_with_index do |pin_id, index|
     eval("PIN_#{index} = PiPiper::Pin.new(pin: #{pin_id}, direction: :out)")
   end
